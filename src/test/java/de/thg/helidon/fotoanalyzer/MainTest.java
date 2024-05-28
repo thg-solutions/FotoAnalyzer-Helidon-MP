2c78bab7cd3762f96d41689a925d85d661bc4e70
Thomas Grund <thomas.grund@thg-solutions.de>
2024-05-28 15:01:52 +0200

    Tests überarbeitet

--- src/test/java/de/thg/helidon/fotoanalyzer/MainTest.java
+++ src/test/java/de/thg/helidon/fotoanalyzer/MainTest.java
@@ -1,21 +1,27 @@
 
 package de.thg.helidon.fotoanalyzer;
 
+import de.thg.helidon.fotoanalyzer.model.Image;
+import io.helidon.metrics.api.MetricsFactory;
+import io.helidon.microprofile.testing.junit5.HelidonTest;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.metrics.Counter;
 import org.eclipse.microprofile.metrics.MetricRegistry;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.core.MediaType;
-
-import io.helidon.microprofile.testing.junit5.HelidonTest;
-import io.helidon.metrics.api.MetricsFactory;
-
-import org.junit.jupiter.api.Test;
+import org.glassfish.jersey.media.multipart.BodyPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.file.StreamDataBodyPart;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.time.LocalDateTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -62,36 +68,21 @@ class MainTest {
         MetricsFactory.closeAll();
     }
 
-
     @Test
-    void testGreet() {
-        Message message = target
-                .path("simple-greet")
+    void testAnalyzeImage() {
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("testdata/PHOTO0021.JPG");
+        MultiPart multiPart = new MultiPart();
+        BodyPart bodyPart = new StreamDataBodyPart("file", inputStream, "PHOTO0021.JPG");
+        multiPart.bodyPart(bodyPart);
+        Image image = target.path("analyze_image")
                 .request()
-                .get(Message.class);
-        assertThat(message.getMessage(), is("Hello World!"));
-    }
-                
-    @Test
-    void testGreetings() {
-        Message jsonMessage = target
-                .path("greet/Joe")
-                .request()
-                .get(Message.class);
-        assertThat(jsonMessage.getMessage(), is("Hello Joe!"));
-
-        try (Response r = target
-                .path("greet/greeting")
-                .request()
-                .put(Entity.entity("{\"greeting\" : \"Hola\"}", MediaType.APPLICATION_JSON))) {
-            assertThat(r.getStatus(), is(204));
-        }
-
-        jsonMessage = target
-                .path("greet/Jose")
-                .request()
-                .get(Message.class);
-        assertThat(jsonMessage.getMessage(), is("Hola Jose!"));
+                .post(Entity.entity(multiPart, MediaType.MULTIPART_FORM_DATA_TYPE), Image.class);
+        Image expected = new Image();
+        expected.setCreationDate(LocalDateTime.of(2015, 11, 22, 11, 04, 37));
+        expected.setLatitude(48.15315222222222);
+        expected.setLongitude(11.591775555555555);
+        expected.setFilename("PHOTO0021.JPG");
+        assertThat(image, equalTo(expected));
     }
                 
 }
